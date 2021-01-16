119bb0645c4188adfa214f6a6c28d9c06b05ed08
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 11:03:05 +0100

    Exercice 6

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercice_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Puis-je avoir votre nom, votre prénom et votre âge ?");
+
+            string v1;
+            v1 = Console.ReadLine();
+            string v2;
+            v2 = Console.ReadLine();
+            int firstNumber;
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Nom : " + v1);
+            Console.WriteLine("Prénom : " + v2);
+            Console.WriteLine("Âge : " + firstNumber);
+
+
+        }
+    }
+}
